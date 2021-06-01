77d4ad0156a63cfbe8c36980ba278693afff798e
samus <samuelnarciso3280@gmail.com>
2021-06-01 12:31:46 -0500

    Resuelve operaciones posrfijas en donde utilizamos variables

--- app.js
+++ app.js
@@ -3,15 +3,76 @@ module.exports = resolverPostfija = (linea = '') => {
     const operadores = {
         ')': { valor: null, simbolo: ')' },
         '(': { valor: null, simbolo: '(' },
-        '^': { valor: 3, funcion: (a, b) => (Math.pow(a, b)), simbolo: '^' },
-        '/': { valor: 2, funcion: (a, b) => (a / b), simbolo: '/' },
-        '*': { valor: 2, funcion: (a, b) => (a * b), simbolo: '*' },
-        '-': { valor: 1, funcion: (a, b) => (a - b), simbolo: '-' },
-        '+': { valor: 1, funcion: (a, b) => (a + b), simbolo: '+' },
+        '^': { valor: 3, funcion: (a, b) => (Math.pow( (a*1), (b*1))), simbolo: '^' },
+        '/': { valor: 2, funcion: (a, b) => ((1*a) / (b*1)), simbolo: '/' },
+        '*': { valor: 2, funcion: (a, b) => ((1*a) * (b*1)), simbolo: '*' },
+        '-': { valor: 1, funcion: (a, b) => ((1*a) - (b*1)), simbolo: '-' },
+        '+': { valor: 1, funcion: (a, b) => ((1*a) + (b*1)), simbolo: '+' },
+    }
+    const alfabeto = {
+        'a': true,
+        'b': true,
+        'c': true,
+        'd': true,
+        'e': true,
+        'f': true,
+        'g': true,
+        'h': true,
+        'i': true,
+        'j': true,
+        'k': true,
+        'l': true,
+        'm': true,
+        'n': true,
+        'ñ': true,
+        'o': true,
+        'p': true,
+        'q': true,
+        'r': true,
+        's': true,
+        't': true,
+        'u': true,
+        'v': true,
+        'w': true,
+        'x': true,
+        'y': true,
+        'z': true,
+
+        'A': true,
+        'B': true,
+        'C': true,
+        'D': true,
+        'E': true,
+        'F': true,
+        'G': true,
+        'H': true,
+        'I': true,
+        'J': true,
+        'K': true,
+        'L': true,
+        'M': true,
+        'N': true,
+        'Ñ': true,
+        'O': true,
+        'P': true,
+        'Q': true,
+        'R': true,
+        'S': true,
+        'T': true,
+        'U': true,
+        'V': true,
+        'W': true,
+        'X': true,
+        'Y': true,
+        'Z': true,
     }
 
+
+    const isaLetter = (letra = '') => (alfabeto[letra] ? true : false)
+
+
     //Metodos para revisar caracter por carac
-    const revisar_caracter = (c) => (operadores[c] || !isNaN(c)) ? true : false
+    const revisar_caracter = (c) => (operadores[c] || !isNaN(c) || isaLetter(c)) ? true : false
     const revisar_linea = (linea = '') => {
         for (const caracter in linea) {
             if (!revisar_caracter(linea[caracter])) return false
@@ -20,20 +81,23 @@ module.exports = resolverPostfija = (linea = '') => {
     }
 
     const armar_input = (linea = "") => {
-        let input_temporal = [], cifra = '', operador = '', linea_arr = linea.split('')
-        while (linea_arr.length > 0) {
-            if (!isNaN(linea_arr[0])) {
-                cifra += linea_arr[0]
+        let input_temporal = [], cifra = '', operador = '', caracteres_arr = linea.split('')
+        while (caracteres_arr.length > 0) {
+            if (!isNaN(caracteres_arr[0]) || isaLetter(caracteres_arr[0])) {
+                // cifra += caracteres_arr[0]
+                cifra += caracteres_arr.shift()
             } else {
-                operador = linea_arr[0]
-                input_temporal.push((cifra * 1), operador)
+                // operador = caracteres_arr[0]
+                operador = caracteres_arr.shift()
+                input_temporal.push(cifra, operador)
                 cifra = '', operador = ''
             }
-            linea_arr.shift()
+            
         }
-        input_temporal.push((cifra * 1), operador)
+        input_temporal.push(cifra, operador)
         return input_temporal.filter((i) => i != '')
     }
+
     const vaciar_pila = () => {
         while (pila.length > 0) {
             const antigu_valor_pila = pila.shift()
@@ -54,13 +118,13 @@ module.exports = resolverPostfija = (linea = '') => {
             vaciar_pila()
             pila.unshift(simbolo_revisar)
         }
-    } 
+    }
     const llenar_pilas = (linea) => {
         for (let i = 0; i < linea.length; i++) {
             const element = linea[i], operador = operadores[element];
             if (!operador) {
-                operacion.push(element * 1)
-                output = output + element * 1
+                operacion.push(element )
+                output = output + element 
                 continue;
             }
             (operador.valor) ? precedencia(operador, pila[0])
@@ -72,10 +136,10 @@ module.exports = resolverPostfija = (linea = '') => {
     const resolver_operacion = () => {
         let arrTmp = []
         while (operacion.length > 0) {
-            while ( !isNaN(operacion[0]) )  arrTmp.unshift(operacion.shift()) 
+            while (!isNaN(operacion[0])) arrTmp.unshift(operacion.shift())
             arrTmp.unshift(operacion.shift())
             let resultado = ''
-            try { resultado = arrTmp[0].funcion((arrTmp[2]), (arrTmp[1])) } 
+            try { resultado = arrTmp[0].funcion((arrTmp[2]), (arrTmp[1])) }
             catch (error) { return null }
 
             for (let i = 0; i < 3; i++) { arrTmp.shift() }
@@ -84,12 +148,16 @@ module.exports = resolverPostfija = (linea = '') => {
         return (arrTmp.length == 1) ? (arrTmp[0].toString() * 1) : null
     }
 
-    if (!revisar_linea(linea)) return { error: true, input: linea, output: output, resultado: null }
+
+
+    if (!revisar_linea(linea)) return { /*error: true */ input: linea, output: output, resultado: null }
+
     const input = armar_input(linea);
+    
     llenar_pilas(input)
     const resultado = resolver_operacion()
     return {
-        error: (resultado != null) ? false : true,
+        // error: (resultado != null) ? false : true,
         input: linea,
         output,
         resultado,
